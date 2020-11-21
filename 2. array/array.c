1139d022ce413361fcc1a97ea989e47040b76712
Tompanepp <tompanepp@gmail.com>
2020-11-21 18:27:09 +0200

    add read and write functionality to the array

--- 2. array/array.c
+++ 2. array/array.c
@@ -11,19 +11,42 @@ array construct(type dataType, int size) {
     switch(dataType) {
         case INT: 
             temp.firstAddress = malloc((sizeof(int) * size));
+            temp.arrayType = dataType;
             break;
         case LONG_INT: 
             temp.firstAddress = malloc((sizeof(long int) * size));
+            temp.arrayType = dataType;
             break;
         case CHAR: 
             temp.firstAddress = malloc((sizeof(char) * size));
+            temp.arrayType = dataType;
             break;
         case FLOAT: 
             temp.firstAddress = malloc((sizeof(float) * size));
+            temp.arrayType = dataType;
             break;
         case DOUBLE: 
             temp.firstAddress = malloc((sizeof(double) * size));
+            temp.arrayType = dataType;
             break;   
     }
     return temp;
 }
+
+/*
+    copmute target memory address
+    dereference address and 
+        set a value at that address
+*/
+void add(array* collection, int index, int value) {
+    void *targetAddress = collection->firstAddress + index;
+    (*(int *)targetAddress) = (int)value; //only dealing with integer for now
+}
+
+int read(array *collection, int index) {
+    //Assuming an integer array
+    void *targetAddress = collection->firstAddress + index;
+    return (int)(*(int *)targetAddress);
+}
+
+
--- 2. array/array.h
+++ 2. array/array.h
@@ -1,6 +1,6 @@
 /*
     1. construct
-    2. ...
+    2. add
 
     Represetation:
         struct Array
@@ -18,8 +18,25 @@
 typedef struct array {
     void* firstAddress;
     int length;
+    type arrayType;
 } array;
 
+/*
+    creates and returns an array for the specified:
+        @dataType = type of values to store
+        @size = number of items to be stored
+*/
 array construct(type dataType, int size);
 
+/*
+    adds a @value at the specified @index
+*/
+void add(array *collection, int index, int value);
+
+/*
+    read and return an item at the specified @index
+*/
+int read(array *collection, int index);
+
+char read(array *collection, int index);
 #endif
