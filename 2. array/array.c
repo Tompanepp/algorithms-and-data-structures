d02550653e1eef8ef72a23352bc18689bf1c33af
Tompanepp <tompanepp@gmail.com>
2021-04-06 00:18:49 +0200

    cast array pointer to the array data type requested and fix naming

--- 2. array/array.c
+++ 2. array/array.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void * allocationMemory(int size);
+void * allocateMemory(int size);
 
 array constructArray(type dataType, int numberOfElements) {
     array temp;
@@ -11,43 +11,43 @@ array constructArray(type dataType, int numberOfElements) {
     temp.arrayType = dataType;
     switch(dataType) {
         case CHAR: 
-            temp.firstAddress = allocationMemory((sizeof(char) * numberOfElements));
+            temp.firstAddress = (char *) allocateMemory((sizeof(char) * numberOfElements));
             break;
         case SIGNED_CHAR: 
-            temp.firstAddress = allocationMemory((sizeof(signed char) * numberOfElements));
+            temp.firstAddress = (signed char *) allocateMemory((sizeof(signed char) * numberOfElements));
             break;
         case UNSIGNED_CHAR: 
-            temp.firstAddress = allocationMemory((sizeof(unsigned char) * numberOfElements));
+            temp.firstAddress = (unsigned char *) allocateMemory((sizeof(unsigned char) * numberOfElements));
             break;
         case INT: 
-            temp.firstAddress = allocationMemory((sizeof(int) * numberOfElements));
+            temp.firstAddress = (int *) allocateMemory((sizeof(int) * numberOfElements));
             break;
         case SHORT_INT: 
-            temp.firstAddress = allocationMemory((sizeof(short int) * numberOfElements));
+            temp.firstAddress = (short int *) allocateMemory((sizeof(short int) * numberOfElements));
             break;
         case LONG_INT: 
-            temp.firstAddress = allocationMemory((sizeof(long int) * numberOfElements));
+            temp.firstAddress = (long int *)allocateMemory((sizeof(long int) * numberOfElements));
             break;
         case SIGNED_INT: 
-            temp.firstAddress = allocationMemory((sizeof(signed int) * numberOfElements));
+            temp.firstAddress = (signed int *) allocateMemory((sizeof(signed int) * numberOfElements));
             break;
         case UNSIGNED_INT: 
-            temp.firstAddress = allocationMemory((sizeof(unsigned int) * numberOfElements));
+            temp.firstAddress = (unsigned int *) allocateMemory((sizeof(unsigned int) * numberOfElements));
             break;
         case FLOAT: 
-            temp.firstAddress = allocationMemory((sizeof(float) * numberOfElements));
+            temp.firstAddress = (float *) allocateMemory((sizeof(float) * numberOfElements));
             break;
         case DOUBLE: 
-            temp.firstAddress = allocationMemory((sizeof(double) * numberOfElements));
+            temp.firstAddress = (double *)allocateMemory((sizeof(double) * numberOfElements));
             break;
         case LONG_DOUBLE: 
-            temp.firstAddress = allocationMemory((sizeof(long double) * numberOfElements));
+            temp.firstAddress = (long double *) allocateMemory((sizeof(long double) * numberOfElements));
             break;
     }
     return temp;
 }
 
-void * allocationMemory(int size) {
+void * allocateMemory(int size) {
     void * firstAddress = malloc(size);
     if(firstAddress != NULL) return firstAddress;
     else {
