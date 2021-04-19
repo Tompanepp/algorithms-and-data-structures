4bb6d16d0ed61c9ca34b4b7fc530c8b43033556c
Tompanepp <tompanepp@gmail.com>
2021-04-19 22:31:18 +0200

    memory test

---
+++ 2. array/test-program-take-too-much-memory.c
@@ -0,0 +1,18 @@
+#include "array.h"
+#include <stdio.h>
+
+int main(){
+    array numbers = constructArray(DOUBLE, 10);
+    printf("number of customer ages %d \n", numbers.length);
+    printf("first address %ld\n", (long int)numbers.firstAddress);
+    printf("adding 2 at index 0\n");
+    add(&numbers, 0, 2);
+    printf("reading a value at index 0 is %d \n", read(&numbers, 0));
+
+    printf("size of double %ld", sizeof(long double));
+    array BIGARRAY = constructArray(LONG_DOUBLE, 33554433);
+    while(1==1) {
+        printf("Just chilling");
+    }
+    return 0;
+}
