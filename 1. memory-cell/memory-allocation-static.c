cbf7c74515e51d5743da54a4b9f197e1aa5a9994
Tompanepp <tompanepp@gmail.com>
2021-05-01 14:08:50 +0200

    move folder back to correct level

---
+++ 1. memory-cell/memory-allocation-static.c
@@ -0,0 +1,14 @@
+/*
+    Fixed memory size defined at compile time.
+    Properties:
+        - Size cannot be increased a runtime
+        - Wastage if allocated size is not fully used
+        - Potentially incorrect program behaviour or crashes if allocated size is exceeded.
+*/
+
+
+//Example
+int main() {
+    int numbers[5] = {1,2,3,4,5}; //Fixed memory size of 5 integers
+    return 0;
+}
