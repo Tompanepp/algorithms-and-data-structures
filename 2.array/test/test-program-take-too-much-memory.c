ee83b919d6f81403fc9fe99112da35037e7a10dd
Tompanepp <tompanepp@gmail.com>
2021-05-22 01:26:41 +0200

    reorganise source

--- 2.array/test/test-program-take-too-much-memory.c
+++ 2.array/test/test-program-take-too-much-memory.c
@@ -8,13 +8,15 @@ int main()
     printf("first address %ld\n", (long int)numbers.first_address);
     printf("adding 2 at index 0\n");
     add(&numbers, 0, 2);
-    printf("reading a value at index 0 is %d \n", read(&numbers, 0));
+    int read_int;
+    read(&numbers, 0, &read_int);
+    printf("reading a value at index 0 is %d \n", read_int);
 
     printf("size of double %ld", sizeof(long double));
     array BIGARRAY = new (LONG_DOUBLE, 33554433);
     while (1 == 1)
     {
-        printf("Just chilling");
+        printf("Just chilling, check memory consumption");
     }
     return 0;
 }
