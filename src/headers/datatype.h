634a40f709faa40b07d0503241a8a7fcaa35fed4
Tompanepp <tompanepp@gmail.com>
2021-05-27 22:11:18 +0200

    rename type.h to same as enum for better alignment

---
+++ src/headers/datatype.h
@@ -0,0 +1,19 @@
+#ifndef TYPE_H
+#define TYPE_H
+
+typedef enum
+{
+    CHAR,
+    SIGNED_CHAR,
+    UNSIGNED_CHAR,
+    INT,
+    SHORT_INT,
+    LONG_INT,
+    SIGNED_INT,
+    UNSIGNED_INT,
+    FLOAT,
+    DOUBLE,
+    LONG_DOUBLE
+} datatype;
+
+#endif
