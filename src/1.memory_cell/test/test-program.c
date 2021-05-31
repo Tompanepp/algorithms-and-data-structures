041765956f1bfb981de11304aec30a247afc2392
Tompanepp <tompanepp@gmail.com>
2021-05-31 21:33:08 +0200

    run memory allocator integration tets

--- src/1.memory_cell/test/test-program.c
+++ src/1.memory_cell/test/test-program.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    printf("Can ask operating system for memory\n");
+    printf("Integration Test - Can ask operating system for memory.\n");
     int *ptr = allocate_memory(sizeof(int));
     *ptr = 2;
     assert(*ptr == 2);
